cbc73c56083a43a078da8f14b8183033e9f2a041
armanbelara <arman@paymongo.com>
2023-04-19 11:11:29 +0800

    Adding ProgramCreate component

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProgramCreate from "./ProgramCreate";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Program</h1>
+      <ProgramCreate />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/ProgramCreate.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const ProgramCreate = () => {
+  const [name, setName] = useState('');
+  const [notes, setNotes] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:4000/programs', {
+      name, notes
+    });
+
+    setName('');
+    setNotes('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            className="form-control"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+        </div>
+        <div className="form-group">
+          <label>Notes</label>
+          <textarea
+            className="form-control"
+            onChange={e => setNotes(e.target.value)}
+            value={notes}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default ProgramCreate;
