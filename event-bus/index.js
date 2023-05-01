c2dc5b43e6629a629f4b72ac590cb202867c0ab2
Arman Belara <armandobelara@gmail.com>
2023-05-01 11:23:58 +0800

    Install cors in event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 
 app.post('/events', (req, res) => {
   const event = req.body;
