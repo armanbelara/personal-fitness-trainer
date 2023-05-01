82bb16f2e7f972ecd143eadd68f42c3e48663545
Arman Belara <armandobelara@gmail.com>
2023-05-01 11:46:25 +0800

    Initial changes for query service

---
+++ query/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const programs = {};
+
+app.get('/programs', (req, res) => {
+  res.send(programs);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "ProgramCreated") {
+    const { id, name, notes } = data;
+
+    programs[id] = { id, name, notes, exercises: [] };
+  }
+
+  if (type === "ProgramExerciseCreated") {
+    const { id, programId, name, sets, reps, weight, unit } = data;
+
+    const program = programs[programId];
+    program.exercises.push({ id, name, sets, reps, weight, unit });
+  }
+
+  console.log(programs);
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log("Listening on 4002");
+});
