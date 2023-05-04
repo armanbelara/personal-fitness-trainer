429901ecb74bd14d28d7795beb81cf8e1813596e
Arman Belara <armandobelara@gmail.com>
2023-05-04 17:14:41 +0800

    Add condition exercise moderation

--- moderation/index.js
+++ moderation/index.js
@@ -5,8 +5,25 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
 
+  if (type === 'ProgramExerciseCreated') {
+
+    const { id, programId, name, sets, reps, weight, unit } = data;
+    const status = name.includes('sex') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'ProgramExerciseModerated',
+      data: {
+        id, programId,
+        name, sets, reps, weight, unit,
+        status
+      }
+    })
+  }
+
+  res.send({});
 });
 
 app.listen(4003, () => {
