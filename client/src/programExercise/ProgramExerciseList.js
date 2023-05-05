8fdce8d229d07270efc2297c06cf4d73fdaa7592
Arman Belara <armandobelara@gmail.com>
2023-05-05 16:29:49 +0800

    Apply rendering of exercise with status

--- client/src/programExercise/ProgramExerciseList.js
+++ client/src/programExercise/ProgramExerciseList.js
@@ -3,10 +3,23 @@ import React from "react";
 const ProgramExerciseList = ({ exercises }) => {
 
   const renderedExercises = exercises.map(exercise => {
+    let content;
+
+    if (exercise.status === 'approved') {
+      const { name, sets, reps, weight, unit } = exercise;
+      content = `${name} - ${sets} sets X ${reps} reps at ${weight} ${unit}`; 
+    }
+
+    if (exercise.status === 'pending') {
+      content = 'This exercise is awaiting moderation.';
+    }
+
+    if (exercise.status === 'rejected') {
+      content = 'This exercise has been rejected.';
+    }
+
     return (
-      <li key={exercise.id}>
-        {exercise.name} - {exercise.sets} sets X {exercise.reps} reps at {exercise.weight} {exercise.unit} 
-      </li>
+      <li key={exercise.id}>{content}</li>
     );
   });
 
