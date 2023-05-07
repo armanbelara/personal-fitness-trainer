1237df9374976bd0991dcb7fcfdeab268f2397a0
Arman Belara <armandobelara@gmail.com>
2023-05-08 07:41:50 +0800

    Implement event synching

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   // Program
   axios.post('http://localhost:4000/events', event)
@@ -33,6 +37,10 @@ app.post('/events', (req, res) => {
     });
   
   res.send({ status: "OK" });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
